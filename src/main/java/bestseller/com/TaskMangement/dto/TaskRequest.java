bc65da7038ad8acca86a636ec2c0d917f598fde1
kardara <abdoulayekardara@gmail.com>
2026-02-25 14:41:25 +0200

    fix: fix task attributes

--- src/main/java/bestseller/com/TaskMangement/dto/TaskRequest.java
+++ src/main/java/bestseller/com/TaskMangement/dto/TaskRequest.java
@@ -2,8 +2,6 @@ package bestseller.com.TaskMangement.dto;
 
 import bestseller.com.TaskMangement.model.ETaskStatus;
 import lombok.*;
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
 @NoArgsConstructor
